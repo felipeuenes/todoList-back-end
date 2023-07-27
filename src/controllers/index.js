cd827b06d660cba702047aacf7ced0131f045a07
Felipe Uenes <uenesdesigner@gmail.com>
2023-07-27 20:19:20 -0300

    add

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -5,13 +5,13 @@ module.exports = {
        
         try {
             
-            const { idtarefas,tarefa, prioridade } = req.body;
+            const { tarefa, prioridade } = req.body;
 
             if (!tarefa) return res.status(400).json('insira uma tarefa!');        
             if (!prioridade) return res.status(400).json('insira a prioridade');
 
 
-            const createTarefa = await prisma.tarefas.create({data: {idtarefas, tarefa, prioridade}});
+            const createTarefa = await prisma.tarefas.create({data: {tarefa, prioridade}});
             return res.status(201).json('Tarefa colocada!')
 
 
