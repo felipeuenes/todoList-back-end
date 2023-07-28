30a791e3dbe38cfe304da0c2f13025255cab2971
Jose-Murilo <murilojucas360@hotmail.com>
2023-07-27 22:00:28 -0300

    added new routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,12 +2,9 @@ const { Router } = require('express')
 const router = Router();
 const controllers = require('../controllers')
 
-
-router.get('/', (req, res) => {
-    res.send('teste de get')
-})
-
+router.get('/', controllers.read)
 router.post('/tarefa', controllers.create)
-
+router.put('/tarefa/:id', controllers.update)
+router.delete('/tarefa/:id', controllers.delete)
 
 module.exports = router;
