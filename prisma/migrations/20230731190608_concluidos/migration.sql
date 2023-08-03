421c1a831a513ae76679dd145316b1892075b45a
Felipe Uenes <uenesdesigner@gmail.com>
2023-08-03 19:27:51 -0300

    testee

--- prisma/migrations/20230731190608_concluidos/migration.sql
+++ prisma/migrations/20230731190608_concluidos/migration.sql
@@ -1,8 +1,8 @@
 -- CreateTable
-CREATE TABLE concluidos (
-    idconcluidos INTEGER NOT NULL AUTO_INCREMENT,
-    tarefa VARCHAR(45) NOT NULL,
-    prioridade VARCHAR(45) NOT NULL,
+CREATE TABLE `concluidos` (
+    `idconcluidos` INTEGER NOT NULL AUTO_INCREMENT,
+    `tarefa` VARCHAR(45) NOT NULL,
+    `prioridade` VARCHAR(45) NOT NULL,
 
-    PRIMARY KEY (idconcluidos)
+    PRIMARY KEY (`idconcluidos`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
