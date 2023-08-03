e05270c0d1407c88242f1b421fce7eee834e9bfc
Felipe Uenes <uenesdesigner@gmail.com>
2023-08-03 19:31:15 -0300

    abc

--- prisma/migrations/20230727234823_tarefas_migration/migration.sql
+++ prisma/migrations/20230727234823_tarefas_migration/migration.sql
@@ -1,8 +1,8 @@
 -- CreateTable
-CREATE TABLE tarefas (
-    idtarefas INTEGER NOT NULL AUTO_INCREMENT,
-    tarefa VARCHAR(45) NOT NULL,
-    prioridade VARCHAR(45) NOT NULL,
+CREATE TABLE `tarefas` (
+    `idtarefas` INTEGER NOT NULL AUTO_INCREMENT,
+    `tarefa` VARCHAR(45) NOT NULL,
+    `prioridade` VARCHAR(45) NOT NULL,
 
-    PRIMARY KEY (idtarefas)
+    PRIMARY KEY (`idtarefas`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
